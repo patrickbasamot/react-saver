7eefb139e71f725417d9032662bdb7a073a0b851
Patrick Basamot <patrickbasamot@gmail.com>
2022-06-13 18:49:26 +0800

    fix fix

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
   return (
     <div className="App">
        <NavBar />
-      <div className='container-fluid'>
-       
+      <div className='container-fluid'>  
         <Product />
        </div>
     </div>
