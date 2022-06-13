9000a4cebea0b5d9be99d34631f174f4052ae315
Patrick Basamot <patrickbasamot@gmail.com>
2022-06-13 18:42:51 +0800

    added NavBar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function NavBar(){
+    return(
+        <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand">Maybelline</a>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+
+    )
+}
+
+export default NavBar;
